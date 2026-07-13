2901709d990c40a2c4c74796e65a93f06c7cb4aa
Ishan Jakkulwar <ishan.jakkulwar@gmail.com>
2026-07-13 13:15:46 +0100

    in the process of adding commments to increase explainability

--- include/mppi_controller/models/drone_state.hpp
+++ include/mppi_controller/models/drone_state.hpp
@@ -1,16 +1,18 @@
-#ifndef MPPI_CONTROLLER__MODELS__DRONE_STATE_HPP_
+// Explained
+
+#ifndef MPPI_CONTROLLER__MODELS__DRONE_STATE_HPP_ //ensures no renaming issues
 #define MPPI_CONTROLLER__MODELS__DRONE_STATE_HPP_
 
-#include <Eigen/Dense>
+#include <Eigen/Dense>// the best library to replace std::vector, can run things SIMD(single instruction multiple data)
 
-namespace mppi_controller
+namespace mppi_controller //create namespace(good for ensuring less errors from different folders/packages clashing)
 {
 
-struct DroneState
+struct DroneState//public version of a class, good for storing valuable, universal data
 {
   Eigen::Vector3d position{0.0, 0.0, 0.0};
   Eigen::Vector3d velocity{0.0, 0.0, 0.0};
-  Eigen::Quaterniond orientation{1.0, 0.0, 0.0, 0.0};  // w, x, y, z
+  Eigen::Quaterniond orientation{1.0, 0.0, 0.0, 0.0};  // w, x, y, z, 4d vector where one part(w)is real, the other 3 are imaginary that encode a rotation axis and angle without gimbal lock 
   Eigen::Vector3d angular_velocity{0.0, 0.0, 0.0};
   double timestamp{0.0};  // seconds
 };
--- include/mppi_controller/utils/math_utils.hpp
+++ include/mppi_controller/utils/math_utils.hpp
@@ -10,18 +10,18 @@ namespace mppi_controller
 namespace math_utils
 {
 
-template<typename T>
+template<typename T>//scalar clamp
 inline T clamp(T value, T min_val, T max_val)
 {
-  return std::max(min_val, std::min(value, max_val));
+  return std::max(min_val, std::min(value, max_val));//evaluates inner paramters first. Finds whether the value is smaller than the max value, and larger than the minimum value
 }
 
-inline Eigen::Vector3d clampVector(
+inline Eigen::Vector3d clampVector(// this is a vector scaling/clamp method. Say you wanted to go diagonally at 100m/s (north and east) but was teuncated to 60m/s, it may go more north than east, this takes care of that
   const Eigen::Vector3d & v, double max_norm)
 {
-  double norm = v.norm();
-  if (norm > max_norm && norm > 1e-9) {
-    return v * (max_norm / norm);
+  double norm = v.norm();// uses pythagorean theorem to find the length of arrow
+  if (norm > max_norm && norm > 1e-9) {//checks whether the norm calculated is greater than max_norm(which is set separelty) and if it is non zero(which is the point of the 1e--9)
+    return v * (max_norm / norm); // if so, it multiplies v by the scale between the two, so say the norm was 5 and max norm was 2, it would be multiplying the vector by 2/5
   }
   return v;
 }
@@ -30,11 +30,11 @@ inline Eigen::Vector3d clampVector(
 // noise. One instance should be owned by whatever controller needs
 // repeated sampling (e.g. MPPI), not constructed fresh every call --
 // std::mt19937 seeding is relatively expensive.
-class GaussianSampler
+class GaussianSampler// this is because computers are incapable of being completely random. Therefore, it fakes being random through an algorithm
 {
-public:
-  explicit GaussianSampler(unsigned int seed = std::random_device{}())
-  : generator_(seed), dist_(0.0, 1.0)
+public://random device uses the random fluctuations in battery health etc. in order to create a truly random number
+  explicit GaussianSampler(unsigned int seed = std::random_device{}())//explicit is very useful as it stops C++ from being too helpful and creating its own gaussian sampler which can take much more cpu and compute and therefore ruin the whole premise 
+  : generator_(seed), dist_(0.0, 1.0)//0.0 indicates it should be close to 0, 1.0 is the spread 
   {
   }
 
